5ed41a97f8d5fb043e31b2218f7db1f6c1dca6e9
Minh NGUYEN <minhstrasbourg@gmail.com>
2024-03-21 10:16:58 +0100

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,50 @@
+import { HeroSection } from "@/components/custom/HeroSection";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"]
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path:string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href)
+
+  try {
+    const response = await fetch(url.href, { cache: 'no-store' })
+    const data = await response.json();
+    const flattenedData = flattenAttributes(data);
+    console.dir(flattenedData, { depth: null });
+    return flattenedData;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+
+  const strapiData = await getStrapiData("/api/home-page")
+
+  const { title, description, blocks } = strapiData;
+
+  return (
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
+}
